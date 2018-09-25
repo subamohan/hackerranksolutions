b2984c2bffd5c4e86e6a8c75fea1eab208a320a6
subamohan <subashini.mohan@crystaldelta.com>
2018-09-25 11:57:35 +0530

    printcontinously file is added in the featurebranch1

---
+++ printcontinously.py
@@ -0,0 +1,23 @@
+#####
+#Note that "" represents the values in between.
+
+###Output Format
+
+#Output the answer as explained in the task.
+
+#Sample Input 0
+
+#3
+
+#Sample Output 0
+
+#123
+
+ from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    for i in range(1,n+1):
+        print (i, end='')
+
+​
